deaa071e68c7e6b5fbbd3430658817fc23135976
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-20 17:38:00 +0900

    빌드 22

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+// 환경변수로 설정된 https://sssssqew.github.io/next-tutorial 뒤에는 자동으로 /가 붙기 때문에 href 시작할때 슬래쉬는 제외함
 export default function Home() {
   return (
     <div>
       <h1>홈 화면</h1>
       <ul>
-        <li><a href={`sub`}>서브 홈</a></li>
-        <li><a href={`sub/about`}>ABOUT</a></li>
-        <li><a href={`sub/1`}>서브 상세 1</a></li>
-        <li><a href={`sub/2`}>서브 상세 2</a></li>
-        <li><a href={`sub/fetch`}>Hello API 접속</a></li>
+        <li><a href='sub'>서브 홈</a></li>
+        <li><a href='sub/about'>ABOUT</a></li>
+        <li><a href='sub/1'>서브 상세 1</a></li>
+        <li><a href='sub/2'>서브 상세 2</a></li>
+        <li><a href='sub/fetch'>Hello API 접속</a></li>
       </ul>
     </div>
   );
--- pages/sub/index.js
+++ pages/sub/index.js
@@ -2,7 +2,7 @@ export default function SubIndex(){
     return (
         <>
             <h1>서브 홈페이지</h1>
-            <a href={`/`}>홈페이지</a>
+            <a href={`${process.env.NEXT_PUBLIC_API_URL}`}>홈페이지</a>
         </>
     )
 }
