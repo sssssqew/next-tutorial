31fe57c0b7d340d25469c5830146b2c252841a8f
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-20 17:30:32 +0900

    빌드 21

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ export default function Home() {
     <div>
       <h1>홈 화면</h1>
       <ul>
-        <li><a href={`/sub`}>서브 홈</a></li>
-        <li><a href={`/sub/about`}>ABOUT</a></li>
-        <li><a href={`/sub/1`}>서브 상세 1</a></li>
-        <li><a href={`/sub/2`}>서브 상세 2</a></li>
-        <li><a href={`/sub/fetch`}>Hello API 접속</a></li>
+        <li><a href={`sub`}>서브 홈</a></li>
+        <li><a href={`sub/about`}>ABOUT</a></li>
+        <li><a href={`sub/1`}>서브 상세 1</a></li>
+        <li><a href={`sub/2`}>서브 상세 2</a></li>
+        <li><a href={`sub/fetch`}>Hello API 접속</a></li>
       </ul>
     </div>
   );
