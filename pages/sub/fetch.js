89abeb46ef788dccfbd29b7595f4d0cc21a3e02f
sssssqew <sunrise.in.my.dream@gmail.com>
2024-05-21 09:45:51 +0900

    빌드 35

--- pages/sub/fetch.js
+++ pages/sub/fetch.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 export default function Fetch(){
     const [data, setData] = useState(null)
     useEffect(() => {
-        fetch(process.env.PRODUCTION_URL +'api/hello')
+        fetch(process.env.NEXT_PUBLIC_API_URL +'api/hello')
         .then(res => res.json())
         .then(result => {
             console.log(result)
